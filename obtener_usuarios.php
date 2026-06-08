11aaae3f52e990c30e88135e02c0ee3eda3d1b04
Lizz-bnet <noemipalacios2003@gmail.com>
2026-06-08 10:43:42 -0500

    obtener_usuarios.php

--- obtener_usuarios.php
+++ obtener_usuarios.php
@@ -1,22 +1,11 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 header('Content-Type: application/json');
 require_once 'config.php';
 
 $sql = "SELECT * FROM usuarios";
-$result = $conn->query($sql);
+$stmt = $conn->query($sql);
 
-if (!$result) {
-    die("Error en SQL: " . $conn->error);
-}
-
-$datos = [];
-
-while($row = $result->fetch_assoc()){
-    $datos[] = $row;
-}
+$datos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($datos);
 ?>
