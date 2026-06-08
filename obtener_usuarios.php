93acc79b161e11d9884bdb32fcc0e471d6e382b4
Lizz-bnet <noemipalacios2003@gmail.com>
2026-06-08 10:32:49 -0500

    obtener_usuarios.php

--- obtener_usuarios.php
+++ obtener_usuarios.php
@@ -1,9 +1,16 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header('Content-Type: application/json');
 require_once 'config.php';
 
 $sql = "SELECT * FROM usuarios";
 $result = $conn->query($sql);
+
+if (!$result) {
+    die("Error en SQL: " . $conn->error);
+}
 
 $datos = [];
 
